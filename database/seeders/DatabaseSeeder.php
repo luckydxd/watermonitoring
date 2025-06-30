cad582767851d41ddf9c01ace15067396882b467
luckydxd <luckydawamuzdzikri@gmail.com>
2025-06-30 23:14:40 +0700

    seeder landing2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AppSettingSeeder::class);
         $this->call(AboutSettingSeeder::class);
         $this->call(PageSeeder::class);
-        $this->call(LAndingPageSeeder::class);
+        $this->call(LandingPageSeeder::class);
 
 
 
